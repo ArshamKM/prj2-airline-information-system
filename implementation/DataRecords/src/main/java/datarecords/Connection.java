44f14b94fe218988290c60866cfdb33a55b89016
ArshamKM <532634@student.fontys.nl>
2024-04-08 11:56:19 +0200

    number 1

---
+++ implementation/DataRecords/src/main/java/datarecords/Connection.java
@@ -0,0 +1,5 @@
+package datarecords;
+
+public class Connection {
+
+}
--- implementation/DataRecords/src/main/java/datarecords/CustomerData.java
+++ implementation/DataRecords/src/main/java/datarecords/CustomerData.java
@@ -1,6 +1,12 @@
 package datarecords;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet; 
+import javax.sql.DataSource;
 
 /**
  * Data carrier for CustomerData.
@@ -9,4 +15,38 @@ import java.time.LocalDate;
  * 
  * @author Informatics Fontys Venlo
  */
-public record CustomerData (int id, String firstName, String lastName, LocalDate dob) {}
+
+    
+
+    
+public record CustomerData (int id, String firstName, String lastName, LocalDate dob) {
+
+    static Map<String, DataSource> cache = new HashMap<>(); 
+
+    static DataSource getDataSource(final String sourceName) {
+
+        return cache.get(sourceName);
+    }
+
+    static CustomerData getCustomerDataFromDatabase(int customerId, String sourceName) { 
+        String query = "SELECT id, first_name, last_name, dob FROM customers WHERE id = ?";
+
+try ( 
+    Connection con = datasource.getConnection(); 
+    PreparedStatement stmt = con.PreparedStatement(query); ) { 
+        int count = 0; 
+        for (Object param : customerData) { 
+            stmt.setObject(++count, param); 
+
+        }
+        return stmt.executeUpdate(); 
+    }
+
+
+
+
+
+
+
+}
+}
